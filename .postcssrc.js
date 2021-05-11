9b5be940150d5521f17ebddaae11f578618eb6bf
“Sunding8080 <1141544903@qq.com>
2021-05-11 23:07:23 +0800

    添加配置文件

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    // to edit target browsers: use "browserslist" field in package.json
+    'postcss-import': {},
+    'postcss-safe-parser': {},
+    'postcss-write-svg': {
+      utf8: false
+    },
+    autoprefixer: {}
+  }
+}
